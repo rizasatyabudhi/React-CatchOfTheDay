d4acb9e73f653151c1ba6062a43da9faf4ac99a5
Unknown <riza.ega.s@mail.ugm.ac.id>
2017-07-31 15:48:04 +0700

    Add lists of fish

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
   constructor() {
@@ -9,6 +11,7 @@ class App extends React.Component {
 
     // This is so our application can use addFish function
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
     // This is how we initialize state, so our application can share states
     this.state = {
       fishes: {},
@@ -17,6 +20,7 @@ class App extends React.Component {
   }
 
   addFish(fish) {
+    // We can use "this" inside this function because we already bind(this) in the constructor
     // Update our state / copy fishes state into new fishes object
     const fishes = { ...this.state.fishes };
     // Add in our new fish to the newly created fishes object
@@ -26,14 +30,28 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes,
+    });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="list-of-fishes">
+            {
+              // We use Object.keys to make our state an array, so then we can map it
+              Object
+                .keys(this.state.fishes)
+                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+            }
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 
       </div>
     );
