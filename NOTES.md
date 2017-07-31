245569115bf1298a740693049537d2907cce5471
Unknown <riza.ega.s@mail.ugm.ac.id>
2017-07-31 13:46:06 +0700

    addFish( )

--- NOTES.md
+++ NOTES.md
@@ -2,3 +2,10 @@
 2. don't put comment above your ELEMENT inside the render()
 3. in react, we want to stay away from touching the DOM as much as possible because the way react works, 
    it just modify the data 
+4. State is just one object that store all of application data
+5. steps to update a state :
+    1. Make a function
+    2. copy our state
+    3. add our new fish to the copied state
+    4. set the newly added fish to the existing original state
+    5. add that function to the constructor 
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,28 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    // This is so our application can use addFish function
+    this.addFish = this.addFish.bind(this);
+    // This is how we initialize state, so our application can share states
+    this.state = {
+      fishes: {},
+      order: {},
+    };
+  }
+
+  addFish(fish) {
+    // Update our state / copy fishes state into new fishes object
+    const fishes = { ...this.state.fishes };
+    // Add in our new fish to the newly created fishes object
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // Same as this.setState ({ fishes : fishes });
+    this.setState({ fishes });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +33,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
 
       </div>
     );
